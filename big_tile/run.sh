c29cfcf0bd16867fe5c83171003dfe00c9df370d
Israt Nisa <nisa.1@buckeyemail.osu.edu>
2018-01-02 14:09:22 -0500

    adding big tile

---
+++ big_tile/run.sh
@@ -0,0 +1,12 @@
+echo "enron"
+./sddmm ../dataset/docword.enron.txt 32 9999999 
+./sddmm ../dataset/docword.enron.txt 128 9999999 
+./sddmm ../dataset/docword.enron.txt 256 9999999 
+echo "kos"
+./sddmm ../dataset/docword.kos.txt 32 9999999 
+./sddmm ../dataset/docword.kos.txt 128 9999999 
+./sddmm ../dataset/docword.kos.txt 256 9999999 
+echo "nips"
+./sddmm ../dataset/docword.nips.txt 32 999999 
+./sddmm ../dataset/docword.nips.txt 128 999999  
+./sddmm ../dataset/docword.nips.txt 256 999999 
